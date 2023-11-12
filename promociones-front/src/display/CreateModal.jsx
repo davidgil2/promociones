2ded00922667d76ab7a4eb76509b87b10adfb705
EstebanC <estebancogo47@gmail.com>
2023-11-11 19:09:59 -0500

    [Completada] Creación de promociones ✅

--- promociones-front/src/display/CreateModal.jsx
+++ promociones-front/src/display/CreateModal.jsx
@@ -10,6 +10,8 @@ import {
 } from "@mui/material";
 import PropTypes from "prop-types";
 import { Checkbox, FormControlLabel } from "@mui/material";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const CreateModal = ({ open, onClose, onCreate }) => {
   CreateModal.propTypes = {
@@ -29,11 +31,34 @@ const CreateModal = ({ open, onClose, onCreate }) => {
   });
 
   const handleCreate = () => {
-    // Lógica para crear datos
-    onCreate(formData);
+    // Validar campos requeridos
+    const requiredFields = [
+      "name",
+      "description",
+      "discountPercentage",
+      "city",
+      "startDate",
+      "endDate",
+    ];
+    const missingFields = requiredFields.filter((field) => !formData[field]);
 
-    // Cierra el modal de creación después de la operación
-    onClose();
+    if (missingFields.length > 0) {
+      toast.error(`Campos obligatorios faltantes: ${missingFields.join(", ")}`);
+      return;
+    }
+
+    // Lógica para crear datos
+    onCreate(formData)
+      .then(() => {
+        // Solicitud exitosa
+        toast.success("Promoción creada exitosamente");
+        onClose();
+      })
+      .catch((error) => {
+        // Solicitud fallida
+        toast.error("Error al crear la promoción");
+        console.error("Error al crear la promoción:", error);
+      });
   };
 
   const handleClose = () => {
@@ -46,94 +71,97 @@ const CreateModal = ({ open, onClose, onCreate }) => {
   };
 
   return (
-    <Dialog open={open} onClose={handleClose}>
-      <DialogTitle>Crear Nueva Promoción</DialogTitle>
-      <DialogContent>
-        <Box
-          component="form"
-          sx={{
-            "& .MuiTextField-root": { m: 1, width: "100%" },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField
-            required
-            id="name"
-            name="name"
-            label="Nombre"
-            variant="outlined"
-            value={formData.name}
-            onChange={(e) => handleChange("name", e.target.value)}
-          />
-          <TextField
-            id="description"
-            name="description"
-            label="Descripción"
-            variant="outlined"
-            value={formData.description}
-            onChange={(e) => handleChange("description", e.target.value)}
-          />
-          <TextField
-            required
-            id="discountPercentage"
-            name="discountPercentage"
-            label="Descuento (%)"
-            variant="outlined"
-            type="number"
-            value={formData.discountPercentage}
-            onChange={(e) => handleChange("discountPercentage", e.target.value)}
-          />
-          <FormControlLabel
-            label="¿Está activada?"
-            control={
-              <Checkbox
-                id="isActive"
-                name="isActive"
-                variant="outlined"
-                checked={formData.isActive}
-                onChange={(e) =>
-                  handleChange("isActive", e.target.checked)
-                }
-              />
-            }
-          />
-          <TextField
-            required
-            id="city"
-            name="city"
-            label="Ciudad"
-            variant="outlined"
-            value={formData.city}
-            onChange={(e) => handleChange("city", e.target.value)}
-          />
-          <TextField
-            required
-            id="startDate"
-            name="startDate"
-            label="Fecha de Inicio 'YYYY-MM-DD HH:mm:ss'"
-            variant="outlined"
-            value={formData.startDate}
-            onChange={(e) => handleChange("startDate", e.target.value)}
-          />
-          <TextField
-            required
-            id="endDate"
-            name="endDate"
-            label="Fecha de finalización 'YYYY-MM-DD HH:mm:ss'"
-            variant="outlined"
-            value={formData.endDate}
-            onChange={(e) => handleChange("endDate", e.target.value)}
-          />
-        </Box>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose}>Cancelar</Button>
-        <Button onClick={handleCreate} color="primary">
-          Crear
-        </Button>
-      </DialogActions>
-    </Dialog>
+    <>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Crear Nueva Promoción</DialogTitle>
+        <DialogContent>
+          <Box
+            component="form"
+            sx={{
+              "& .MuiTextField-root": { m: 1, width: "100%" },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField
+              required
+              id="name"
+              name="name"
+              label="Nombre"
+              variant="outlined"
+              value={formData.name}
+              onChange={(e) => handleChange("name", e.target.value)}
+            />
+            <TextField
+              id="description"
+              name="description"
+              label="Descripción"
+              variant="outlined"
+              value={formData.description}
+              onChange={(e) => handleChange("description", e.target.value)}
+            />
+            <TextField
+              required
+              id="discountPercentage"
+              name="discountPercentage"
+              label="Descuento (%)"
+              variant="outlined"
+              type="number"
+              value={formData.discountPercentage}
+              onChange={(e) =>
+                handleChange("discountPercentage", e.target.value)
+              }
+            />
+            <FormControlLabel
+              label="¿Está activada?"
+              control={
+                <Checkbox
+                  id="isActive"
+                  name="isActive"
+                  variant="outlined"
+                  checked={formData.isActive}
+                  onChange={(e) => handleChange("isActive", e.target.checked)}
+                />
+              }
+            />
+            <TextField
+              required
+              id="city"
+              name="city"
+              label="Ciudad"
+              variant="outlined"
+              value={formData.city}
+              onChange={(e) => handleChange("city", e.target.value)}
+            />
+            <TextField
+              required
+              id="startDate"
+              name="startDate"
+              label="Fecha de Inicio 'YYYY-MM-DD HH:mm:ss'"
+              variant="outlined"
+              value={formData.startDate}
+              onChange={(e) => handleChange("startDate", e.target.value)}
+            />
+            <TextField
+              required
+              id="endDate"
+              name="endDate"
+              label="Fecha de finalización 'YYYY-MM-DD HH:mm:ss'"
+              variant="outlined"
+              value={formData.endDate}
+              onChange={(e) => handleChange("endDate", e.target.value)}
+            />
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancelar</Button>
+          <Button onClick={handleCreate} color="primary">
+            Crear
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <ToastContainer />
+    </>
   );
 };
 
